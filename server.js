7327789b4975bb851659ffc53114e631ca265e18
Kazi Islam <kazichaska@gmail.com>
2022-02-11 16:47:26 -0600

    API delete call

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ const notesData = require("./db/db.json");
 const path = require("path");
 const uuid = require('uuid');
 const res = require("express/lib/response");
+const { randomUUID } = require('crypto');
+console.log(randomUUID());
 
 
 const app = express();
@@ -30,8 +32,8 @@ app.get("/api/notes", (req, res) => {
 });
 
 app.post("/api/notes", (req, res) => {
-    notesData.push(req.body);
-    console.log(notesData);
+    notesData.push({ id: randomUUID(), ...req.body});
+    console.log({ id: randomUUID(), ...req.body});
     fs.writeFileSync("./db/db.json", JSON.stringify(notesData, null, 2));
     res.json(notesData);
 })
@@ -39,9 +41,14 @@ app.post("/api/notes", (req, res) => {
 app.delete("/api/notes/:id", (req, res) => {
     console.log("Req params", req.params.id) 
         const dbIndex = notesData.findIndex(({ id }) => id === req.params.id);
-        if (dbIndex >= 0) {
-            notesData.splice(dbIndex, 1);
-        }
+        console.log(dbIndex);
+        delete notesData[dbIndex];
+        // notesData.splice(dbIndex);
+        // if (dbIndex > -1) {
+        //     notesData.splice(dbIndex);
+        // }
+        // res.end(JSON.stringify(notesData, null, 2));
+        // notesData.remove(dbIndex);
 });
 
 // app.delete('/api/notes/:id', (req, res) => {
