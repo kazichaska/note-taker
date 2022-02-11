c37be51b8419d2f4eeb5e72bcc02639f60b1228c
Kazi Islam <kazichaska@gmail.com>
2022-02-11 11:00:57 -0600

    add GET and POST routes

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ const fs = require("fs");
 const PORT = process.env.PORT || 3001;
 const notesData = require("./db/db.json");
 const path = require("path");
+const uuid = require('uuid');
+const res = require("express/lib/response");
+
 
 const app = express();
 app.use(express.static("public"));
-app.use(express.json());
+
+// parse incoming string or array data
 app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "./public/index.html"));
@@ -29,6 +35,18 @@ app.post("/api/notes", (req, res) => {
     fs.writeFileSync("./db/db.json", JSON.stringify(notesData, null, 2));
     res.json(notesData);
 })
+
+app.delete("/api/notes/:id", (req, res) => {
+    console.log("Req params", req.params.id) 
+        const dbIndex = notesData.findIndex(({ id }) => id === req.params.id);
+        if (dbIndex >= 0) {
+            notesData.splice(dbIndex, 1);
+        }
+});
+
+// app.delete('/api/notes/:id', (req, res) => {
+//     res.send(`${req.method} Delete request to db.json`);
+// });
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port http://localhost:${PORT}!`);
